c8a3a1dcf96dcb91812cfb88b5772f4f025f4894
정그린 <th2gr22n@gmail.com>
2024-07-27 19:44:33 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.js
+++ 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.js
@@ -1,13 +1,7 @@
-function solution(num1, num2) {
-    // if num1==num2:
-    //     print('1')
-    // else:
-    //     print('-1')
-    
-    if(num1==num2){
-    return 1
-    }
-    else{
-    return -1
+function solution(num1, num2) {    
+    if (num1 == num2) {
+        return 1
+    } else {
+        return -1
     }
 }
