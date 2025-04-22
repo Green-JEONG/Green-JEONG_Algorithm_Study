65a928707f9267fbee21ebd49fed0c1543b132f5
정그린 <th2gr22n@gmail.com>
2025-04-22 13:50:34 +0900

    [Bronze V] Title: 오늘 날짜, Time: 32 ms, Memory: 34592 KB -BaekjoonHub

--- 백준/Bronze/10699. 오늘 날짜/오늘 날짜.py
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.py
@@ -1,5 +1,4 @@
-from datetime import datetime, timedelta, timezone
+from datetime import date
 
-KST = timezone(timedelta(hours=9))  # 한국 표준시 (KST) 설정
-today = datetime.now(KST)  # 현재 한국 표준시로 시간 가져오기
-print(today.strftime("%Y-%m-%d"))  # YYYY-MM-DD 형식으로 출력
+today = date.today()
+print(today)
