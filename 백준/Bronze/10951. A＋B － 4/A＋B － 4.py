ae20f92ed26152cd98ff9094dc32cde1449d74cf
정그린 <th2gr22n@gmail.com>
2025-04-22 19:09:05 +0900

    [Bronze V] Title: A+B - 4, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10951. A＋B － 4/A＋B － 4.py
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.py
@@ -1,6 +1,6 @@
 while True:
-    try: # 입력을 받는 부분, 입력 없을 때 예외 처리
+    try:
         A, B = map(int, input().split())
         print(A+B)
-    except: # 입력이 없을 때 예외 (에러 발생 시 루프 종료)
+    except EOFError:
         break
