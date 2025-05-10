b7049eee093e065c659bd8e87cf96184caabdbd3
정그린 <th2gr22n@gmail.com>
2025-05-10 20:10:51 +0900

    [Bronze V] Title: X보다 작은 수, Time: 40 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.py
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.py
@@ -1,6 +1,8 @@
-N, X = map(int, input().split())
-A = list(map(int, input().split()))    # 리스트 안 문자열을 int로 만든 map 객체를 진짜 list로 바꾸기
+import sys
+
+n, x = map(int, sys.stdin.readline().split())
+a = list(map(int, input().split()))
   
-for i in A:    # A 안에서
-    if i < X:    # X보다 작은 값 찾기
-        print(i, end=' ')    # 줄바꿈을 공백으로 바꾸기
+for i in a:
+    if i < x:
+        print(i, end=' ')
