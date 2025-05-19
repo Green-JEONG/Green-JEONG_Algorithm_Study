32a36060ae1777f62a1d2c8f7f54251b5f10425a
정그린 <th2gr22n@gmail.com>
2025-05-19 22:31:02 +0900

    [Bronze III] Title: 짝수를 찾아라, Time: 88 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/3058. 짝수를 찾아라/짝수를 찾아라.py
+++ 백준/Bronze/3058. 짝수를 찾아라/짝수를 찾아라.py
@@ -2,13 +2,5 @@ t = int(input())
 
 for _ in range(t):
     nums = list(map(int, input().split()))
-    
-    even = []
-    even_sum = 0
-    for n in nums:
-        if n % 2 == 0:
-            even.append(n)
-            even_sum += n
-    
-    
-    print(even_sum, min(even))
+    even = [n for n in nums if n % 2 == 0]
+    print(sum(even), min(even))
