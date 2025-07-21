50852ac7f7957e9d7a6a5d1977f12180b6fab1d5
정그린 <th2gr22n@gmail.com>
2025-07-21 19:29:01 +0900

    [level 0] Title: 7의 개수, Time: 0.01 ms, Memory: 9.16 MB -BaekjoonHub

--- 프로그래머스/0/120912. 7의 개수/7의 개수.py
+++ 프로그래머스/0/120912. 7의 개수/7의 개수.py
@@ -1,8 +1,2 @@
 def solution(array):
-    count = 0
-    for n in array:
-        for ch in str(n):
-            if ch == '7':
-                count += 1
-        
-    return count
+    return str(array).count('7')
