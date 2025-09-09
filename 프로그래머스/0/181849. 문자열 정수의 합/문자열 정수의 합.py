3bdeb45739694a652b5db3fc394f9bcb6fddc57a
정그린 <th2gr22n@gmail.com>
2025-09-09 23:56:33 +0900

    [level 0] Title: 문자열 정수의 합, Time: 0.05 ms, Memory: 9.26 MB -BaekjoonHub

---
+++ 프로그래머스/0/181849. 문자열 정수의 합/문자열 정수의 합.py
@@ -0,0 +1,10 @@
+def solution(num_str):
+#     total = 0
+#     for ch in num_str:
+#         total += int(ch)
+        
+#     return total
+        
+    return sum([int(ch) for ch in num_str])
+
+    
