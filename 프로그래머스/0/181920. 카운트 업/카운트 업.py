3e633371454465572505fa22453065e4a93a0855
정그린 <th2gr22n@gmail.com>
2025-09-10 23:49:20 +0900

    [level 0] Title: 카운트 업, Time: 0.01 ms, Memory: 9.26 MB -BaekjoonHub

---
+++ 프로그래머스/0/181920. 카운트 업/카운트 업.py
@@ -0,0 +1,8 @@
+def solution(start_num, end_num):
+#     result = []
+#     for ch in range(start_num, end_num + 1):
+#         result.append(ch)
+        
+#     return result
+
+    return [ch for ch in range(start_num, end_num + 1)]
