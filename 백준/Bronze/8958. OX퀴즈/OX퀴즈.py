c23ce707c8873a94d018c3854e1ce1d80feb9bf9
정그린 <th2gr22n@gmail.com>
2025-09-21 01:47:32 +0900

    [Bronze II] Title: OX퀴즈, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/8958. OX퀴즈/OX퀴즈.py
+++ 백준/Bronze/8958. OX퀴즈/OX퀴즈.py
@@ -1,15 +1,10 @@
-n = int(input())
-
-for _ in range(n):
-    str = input()
-    score = 0    # 총 점수
-    consec = 0    # 연속 O 점수
-    
-    for ch in str:
-        if ch == "O":
+t = int(input())
+for _ in range(t):
+    total = consec = 0
+    for ch in input():
+        if ch == 'O':
             consec += 1
-            score += consec
+            total += consec
         else:
-            consec = 0    # X 나올 시, 연속 끊기
-       
-    print(score)
+            consec = 0
+    print(total)
