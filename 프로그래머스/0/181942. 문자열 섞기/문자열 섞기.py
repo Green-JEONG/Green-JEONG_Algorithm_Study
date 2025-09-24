dd3af2db53078642c95531782712d63875c5c9df
정그린 <th2gr22n@gmail.com>
2025-09-24 21:10:50 +0900

    [level 0] Title: 문자열 섞기, Time: 0.01 ms, Memory: 9.14 MB -BaekjoonHub

--- 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.py
+++ 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.py
@@ -1,10 +1,5 @@
 def solution(str1, str2):
-
-    result = ""
-    
-    min_len = min(len(str1), len(str2))
-    
-    for i in range(min_len):
-        result += str1[i] + str2[i]
-        
-    return result
+    answer = ''
+    for i in range(len(str1)):
+        answer += str1[i] + str2[i]
+    return answer
