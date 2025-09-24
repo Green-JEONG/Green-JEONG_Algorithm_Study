00c8aaf8540c2ea5f3f4045baf8fa02a1a5dfa90
정그린 <th2gr22n@gmail.com>
2025-09-25 01:26:02 +0900

    [level 0] Title: 원소들의 곱과 합, Time: 0.00 ms, Memory: 9.08 MB -BaekjoonHub

--- 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.py
+++ 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.py
@@ -1,3 +1,5 @@
 def solution(num_list):
-    from math import prod
-    return 1 if prod(num_list) < sum(num_list) ** 2 else 0
+    multi = 1
+    for i in num_list:
+        multi *= i
+    return 1 if sum(num_list)**2 > multi else 0
