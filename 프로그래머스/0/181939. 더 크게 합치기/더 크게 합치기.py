0b328111e41d6b5a7074107abcdca5ed9248dd3d
정그린 <th2gr22n@gmail.com>
2025-09-24 21:45:44 +0900

    [level 0] Title: 더 크게 합치기, Time: 0.03 ms, Memory: 9.29 MB -BaekjoonHub

--- 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.py
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.py
@@ -1,4 +1,2 @@
 def solution(a, b):
-    asc_s = int(str(a) + str(b))
-    desc_s = int(str(b) + str(a))
-    return desc_s if asc_s < desc_s else asc_s
+    return max(int(f'{a}{b}'), int(f'{b}{a}'))
