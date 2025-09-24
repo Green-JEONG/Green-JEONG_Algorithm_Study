eea3ec139c591a3ff4131d6033029b55c4704e61
정그린 <th2gr22n@gmail.com>
2025-09-24 09:29:28 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.03 ms, Memory: 9.36 MB -BaekjoonHub

--- 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.py
+++ 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.py
@@ -1,6 +1,7 @@
 def solution(n):
+    # total = 0
+    # for i in str(n):
+    #     total += int(i)
+    # return total
     
-    total = 0
-    for i in str(n):
-        total += int(i)
-    return total
+    return sum(int(i) for i in str(n))
