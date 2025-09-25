2c96946da30ba94248e8ccd02d4c30b5a36711a3
정그린 <th2gr22n@gmail.com>
2025-09-26 01:43:00 +0900

    [level 1] Title: 최소직사각형, Time: 3.54 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
@@ -0,0 +1,10 @@
+def solution(sizes):
+    for i in range(len(sizes)):
+        w, h = sizes[i]
+        if w < h:
+            sizes[i] = [h, w] # 명함 돌려서 항상 가로가 더 크게 맞추기
+            
+    max_w = max(w for w, h in sizes)
+    max_h = max(h for w, h in sizes)
+    
+    return max_w * max_h
