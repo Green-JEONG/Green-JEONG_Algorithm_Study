961c830c9a5dc0602d4294655f6f349b82cb7459
정그린 <th2gr22n@gmail.com>
2025-09-27 03:19:06 +0900

    [level 2] Title: 전화번호 목록, Time: 116.10 ms, Memory: 29.9 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,6 +1,6 @@
 def solution(phone_book):
     phone_book.sort()
-    for i in range(len(phone_book) - 1):
+    for i in range(len(phone_book) - 1): # 마지막 원소 직전까지만 확인하기 위해 1 빼주기
         if phone_book[i+1].startswith(phone_book[i]):
             return False
     return True # 모든 비교 끝난 뒤 실행
