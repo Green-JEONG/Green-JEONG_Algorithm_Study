fa090dcee2979aa1631bcfdf52006267cd8e4450
정그린 <th2gr22n@gmail.com>
2025-09-27 01:23:46 +0900

    [level 2] Title: 모음 사전, Time: 0.01 ms, Memory: 9.11 MB -BaekjoonHub

---
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -0,0 +1,10 @@
+def solution(word):
+    # 마지막 글자만 바뀌면 순서가 그 뒤
+    vowels = ['A', 'E', 'I', 'O', 'U']
+    jumps = [781, 156, 31, 6, 1]
+    
+    order = 0
+    for i, ch in enumerate(word):
+        order += vowels.index(ch) * jumps[i] + 1 # 해당 글자가 몇 번째 알파벳인지 찾고, 그 위치에서 몇 칸이나 건너뛰는지 계산한 값에 자기 자신(+1)을 더한다.
+        
+    return order
