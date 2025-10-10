49ad959dbe88d020930c50c23de78ce28e3a9756
정그린 <th2gr22n@gmail.com>
2025-10-11 00:13:46 +0900

    [level 0] Title: 접미사 배열, Time: 0.06 ms, Memory: 9.21 MB -BaekjoonHub

--- 프로그래머스/0/181909. 접미사 배열/접미사 배열.py
+++ 프로그래머스/0/181909. 접미사 배열/접미사 배열.py
@@ -1,2 +1,8 @@
 def solution(my_string):
-    return sorted([my_string[i:] for i in range(len(my_string))])
+    result = []
+    
+    for i in range(len(my_string)):
+        result.append(str(my_string[i:]))
+        i += 1
+        
+    return sorted(result)
