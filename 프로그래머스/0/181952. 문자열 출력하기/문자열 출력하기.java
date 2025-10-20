e8cac1edf1b8ebbd85fdffeeb085d050aba53a27
정그린 <th2gr22n@gmail.com>
2025-10-21 00:08:09 +0900

    [level 0] Title: 문자열 출력하기, Time: 174.93 ms, Memory: 73.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181952. 문자열 출력하기/문자열 출력하기.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); // 입력 받기 위한 Scanner 생성
+        String str = sc.nextLine(); // 문자열 한 단어 입력받기
+        System.out.println(str);
+    }
+}
