e5e4bc49b3550a6e7204e0b139345a3df479fc61
정그린 <th2gr22n@gmail.com>
2025-10-28 18:04:18 +0900

    [level 1] Title: 소수 찾기, Time: 3220.74 ms, Memory: 9.35 MB -BaekjoonHub

---
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.py
@@ -0,0 +1,16 @@
+def solution(n):
+    def is_prime(n):
+        if n < 2:
+            return False
+        for i in range(2, int(n**0.5)+1):
+            if n % i == 0:
+                return False
+        return True
+    
+    cnt = 0
+    
+    for i in range(2, n+1):
+        if is_prime(i):
+            cnt += 1
+    
+    return cnt
